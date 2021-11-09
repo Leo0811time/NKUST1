2ff6f6488803e1c7cc88c0496bfbc940f80f3256
Leo0811time <93572177+Leo0811time@users.noreply.github.com>
2021-11-09 16:30:12 +0800

    Add files via upload

---
+++ math.java
@@ -0,0 +1,13 @@
+public class math {
+    public static void main(String[] args) {
+
+        int hight = 180;
+        int weight = 97;
+
+
+        int BMI =weight/((hight/100)^2);
+
+            System.out.println(BMI);
+    }
+
+}
---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args){
+    // This is a comment
+        //lowerCamelCase variable name
+        String myName = "leo lee";
+                System.out.println("Hello "+myName);
+    }
+}
